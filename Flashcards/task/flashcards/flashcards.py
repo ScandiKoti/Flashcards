5e2c10079f8ac370ed970ffe8bbd35618ecf2bd4
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2023-02-01 00:29:26 +0300

    Stage 3/7: Make it your own

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,2 +1,26 @@
-term, definition, answer = (input() for _ in range(3))
-print("Your answer is right!" if answer == definition else "Your answer is wrong...")
+def check_user_input():
+    while True:
+        number_of_cards = input('Input the number of cards:\n')
+        try:
+            number_of_cards = int(number_of_cards)
+            if number_of_cards < 1:
+                raise ValueError
+        except (ValueError, IndexError):
+            print('Invalid input!')
+            continue
+        break
+    return number_of_cards
+
+
+def main():
+    number_of_cards = check_user_input()
+    cards_dict = {}
+    for i in range(1, number_of_cards + 1):
+        term, definition = input(f'The term for card #{i}:\n'), input(f'The definition for card #{i}:\n')
+        cards_dict.update({term: definition})
+    for k, v in cards_dict.items():
+        print('Correct!' if input(f'Print the definition of "{k}":\n') == v else f'Wrong. The right answer is "{v}"')
+
+
+if __name__ == '__main__':
+    main()
