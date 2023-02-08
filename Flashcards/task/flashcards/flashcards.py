2ed00498353a7ea197c34b332287b7abfef96db7
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2023-02-09 00:08:47 +0300

    Stage 4/7: A good stack

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,25 +1,56 @@
-def check_user_input():
-    while True:
-        number_of_cards = input('Input the number of cards:\n')
-        try:
-            number_of_cards = int(number_of_cards)
-            if number_of_cards < 1:
-                raise ValueError
-        except (ValueError, IndexError):
-            print('Invalid input!')
-            continue
-        break
-    return number_of_cards
+class FlashCards:
+    def __init__(self):
+        self.number_of_cards = 0
+        self.cards_dict = {}
+
+    def check_user_input(self):
+        while True:
+            try:
+                self.number_of_cards = int(input('Input the number of cards:\n'))
+                if self.number_of_cards < 1:
+                    raise ValueError
+            except (ValueError, IndexError):
+                print('Invalid input!')
+                continue
+            break
+
+    def add_card(self):
+        for i in range(1, self.number_of_cards + 1):
+            term = input(f'The term for card #{i}:\n')
+            while True:
+                for k in self.cards_dict:
+                    if term == k:
+                        term = input(f'The term "{term}" already exists. Try again:\n')
+                        continue
+                break
+            definition = input(f'The definition for card #{i}:\n')
+            while True:
+                for v in self.cards_dict.values():
+                    if definition == v:
+                        definition = input(f'The definition "{definition}" already exists. Try again:\n')
+                        continue
+                break
+            self.cards_dict[term] = definition
+
+    def check_answer(self):
+        for k, v in self.cards_dict.items():
+            answer = input(f'Print the definition of "{k}":\n')
+            if answer == v:
+                print('Correct!')
+            else:
+                message = f'Wrong. The right answer is "{v}".'
+                for term, definition in self.cards_dict.items():
+                    if answer == definition:
+                        message = f'Wrong. The right answer is "{v}", but your definition is correct for "{term}".'
+                        break
+                print(message)
 
 
 def main():
-    number_of_cards = check_user_input()
-    cards_dict = {}
-    for i in range(1, number_of_cards + 1):
-        term, definition = input(f'The term for card #{i}:\n'), input(f'The definition for card #{i}:\n')
-        cards_dict.update({term: definition})
-    for k, v in cards_dict.items():
-        print('Correct!' if input(f'Print the definition of "{k}":\n') == v else f'Wrong. The right answer is "{v}"')
+    game = FlashCards()
+    game.check_user_input()
+    game.add_card()
+    game.check_answer()
 
 
 if __name__ == '__main__':
